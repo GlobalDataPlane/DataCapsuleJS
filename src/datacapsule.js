bea3bf6d5562a673069dab42de0c42de70f6af40
William Mullen <wmullen@berkeley.edu>
2021-11-15 00:48:05 -0800

    Added serialization compatibility and test prep

--- src/datacapsule.js
+++ src/datacapsule.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 var crypto = require('crypto');
-const hash = crypto.createHash('sha256');
 
 class CapsuleRecord {
     constructor(data) {
         this.previousRecord;
         this.previousHash;
-        this.dataHash = hash.update(data).digest('hex');
+        this.dataHash = crypto.createHash('sha256').update(data).digest('hex');
         this.data = data;
 
         this.headerHash;
@@ -23,69 +22,84 @@ class DataCapsule {
 
         this.recentRecord;
         // Hash over basic data to generate GDPname
-        this.name = hash.update(this.ownerKey + this.protocol + this.version + this.encodingScheme + this.instanceID).digest('hex');
+        this.name = crypto.createHash('sha256').update(this.ownerKey + this.protocol + this.version + this.encodingScheme + this.instanceID).digest('hex');
         
         // Initial record
-        var newRec = new CapsuleRecord(data);      
+        var newRec = new CapsuleRecord("");      
         newRec.previousHash = "";
-        newRec.headerHash = hash.update(newRec.dataHash + newRec.previousHash).digest('hex');
-        const sign = createSign('SHA256');
+        newRec.headerHash = crypto.createHash('sha256').update(newRec.previousHash + newRec.dataHash).digest('hex');
+        const sign = crypto.createSign('sha256');
         sign.update(newRec.headerHash);
         sign.end();
-        const signature = sign.sign(ownerKey);
+        const signature = sign.sign(ownerKey, 'hex');
         newRec.signature = signature;
         this.recentRecord = newRec;
 
-    }
-
-    validateRecord(verifyKey, record) {
-        const verify = createVerify('SHA256');
-        verify.update(record.headerHash);
-        verify.end();
-        var sigVerify = verify.verify(verifyKey, record.signature);
-
-        var recHash = hash.update(record.previousHash + record.dataHash).digest('hex');
-        var hashVerify = recHash !== lastRec.headerHash;
-
-        return sigVerify && hashVerify;
-    }
-
-    readLast(verifyKey) {
-        if (this.validateRecord(verifyKey, this.recentRecord)) {
-            return this.recentRecord;
-        }
-        return "Validation failed.";
-    }
-
-    write(signKey, verifyKey, data) {
-        var lastRec = this.recentRecord;
-
-        // Verify previous record's authenticity
-        if (!this.validateRecord(verifyKey, this.recentRecord)) {
-            return "Validation fail, check records."
-        }        
-
-        // Create new CapsuleRecord
-        var newRec = new CapsuleRecord(data);      
-
-        // Add prev record data for Capsule record
-        newRec.previousHash = lastRec.headerHash;
-        newRec.previousRecord = lastRec;
-
-        // Generate header hash
-        newRec.headerHash = hash.update(newRec.dataHash + newRec.previousHash).digest('hex');
-
-        // Sign record
-        const sign = createSign('SHA256');
-        sign.update(newRec.headerHash);
-        sign.end();
-        const signature = sign.sign(signKey);
-        newRec.signature = signature;
-
-        // Append and update DataCapsule
-        this.recentRecord = newRec;
-
-        // Return hash of record
-        return newRec.headerHash;
     }
 }
+
+function validateRecord(record, verifyKey) {
+    const verify = crypto.createVerify('SHA256');
+    verify.update(record.headerHash);
+    verify.end();
+    var sigVerify = verify.verify(verifyKey, record.signature, 'hex');
+
+    var recHash = crypto.createHash('sha256').update(record.previousHash + record.dataHash).digest('hex');
+    var hashVerify = (recHash === record.headerHash);
+    return sigVerify && hashVerify;
+}
+
+function dcReadLast(dataCapsule, verifyKey) {
+    if (validateRecord(dataCapsule.recentRecord, verifyKey)) {
+        return dataCapsule.recentRecord;
+    }
+    return "Validation failed.";
+}
+
+function dcWrite(dataCapsule, signKey, verifyKey, data) {
+    var lastRec = dataCapsule.recentRecord;
+    
+    // Verify previous record's authenticity
+    if (!validateRecord(dataCapsule.recentRecord, verifyKey)) {
+        return "Validation fail, check records."
+    }        
+    
+    // Create new CapsuleRecord
+    var newRec = new CapsuleRecord(data);      
+
+    // Add prev record data for Capsule record
+    newRec.previousHash = lastRec.headerHash;
+    newRec.previousRecord = lastRec;
+    // Generate header hash
+    newRec.headerHash = crypto.createHash('sha256').update(newRec.previousHash + newRec.dataHash).digest('hex');
+
+    // Sign record
+    const sign = crypto.createSign('SHA256');
+    sign.update(newRec.headerHash);
+    sign.end();
+    const signature = sign.sign(signKey, 'hex');
+    newRec.signature = signature;
+
+    // Append and update DataCapsule
+    dataCapsule.recentRecord = newRec;
+
+    // Return hash of record
+    return newRec.headerHash;
+}
+
+
+
+const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+});
+var testCapsule = new DataCapsule(privateKey, "TestProt", "v1", "testscheme", "12");
+// console.log(testCapsule);
+// console.log(testCapsule.signature);
+// var capsuleString = JSON.stringify(testCapsule);
+// console.log(capsuleString);
+// var restoredCapsule = JSON.parse(capsuleString);
+// console.log(restoredCapsule);
+// restoredCapsule.signature = Buffer(restoredCapsule.signature.data);
+dcWrite(testCapsule, privateKey, publicKey, "hello this is a test");
+// console.log(testCapsule);
+console.log(dcReadLast(testCapsule, publicKey).data);
